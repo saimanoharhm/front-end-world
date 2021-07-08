d5d4432e400edbbf51991fe01254b944e1c18b2d
ANNAM SAI MANOHAR <saimanohar987@gmail.com>
2021-07-08 23:21:58 +0530

    feat: Add today's practice work

--- React/expense-tracker/src/components/Expenses/Expenses.js
+++ React/expense-tracker/src/components/Expenses/Expenses.js
@@ -1,29 +1,33 @@
-import ExpenseItem from "./ExpenseItem";
-import ExpensesFilter from "./ExpensesFilter";
-import Card from "../UI/Card";
-import "./Expenses.css";
 import { useState } from "react";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import "./Expenses.css";
+import ExpensesChart from "./ExpensesChart";
+
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2021");
+
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter
           selected={filteredYear}
-          onChangeHandler={filterChangeHandler}
+          onChangeFilter={filterChangeHandler}
         />
-        {props.items.map((expense) => (
-          <ExpenseItem
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
 };
+
 export default Expenses;
--- React/expense-tracker/src/components/Expenses/ExpensesFilter.js
+++ React/expense-tracker/src/components/Expenses/ExpensesFilter.js
@@ -1,14 +1,15 @@
 import "./ExpensesFilter.css";
 
 const ExpensesFilter = (props) => {
-  const dropDownclickHandler = (event) => {
-    props.onChangeHandler(event.target.value);
+  const dropdownChangeHandler = (event) => {
+    props.onChangeFilter(event.target.value);
   };
+
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select value={props.selected} onChange={dropDownclickHandler}>
+        <select value={props.selected} onChange={dropdownChangeHandler}>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
