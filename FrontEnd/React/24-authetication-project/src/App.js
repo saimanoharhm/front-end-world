f136a35e954f6b726561dee76842559e233a516c
Annam Sai Manohar <saimanohar987@gmail.com>
2022-02-02 09:09:54 +0530

    feat: Add Today's Work!

--- FrontEnd/React/24-authetication-project/src/App.js
+++ FrontEnd/React/24-authetication-project/src/App.js
@@ -1,21 +1,20 @@
-import { Switch, Route } from 'react-router-dom';
-
-import Layout from './components/Layout/Layout';
-import UserProfile from './components/Profile/UserProfile';
-import AuthPage from './pages/AuthPage';
-import HomePage from './pages/HomePage';
+import { Switch, Route } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import UserProfile from "./components/Profile/UserProfile";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <Layout>
       <Switch>
-        <Route path='/' exact>
+        <Route path="/" exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
+        <Route path="/auth">
           <AuthPage />
         </Route>
-        <Route path='/profile'>
+        <Route path="/profile">
           <UserProfile />
         </Route>
       </Switch>
